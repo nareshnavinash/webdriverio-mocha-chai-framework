e4116236662dcda3b5cf5e5def6388832e2735c8
Naresh Sekar <naresh@madstreetden.com>
2020-07-23 15:10:16 +0530

    framework skeleton commit

---
+++ utilities/Utilities.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+class Utilities {
+  static getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  static takeScreenshot(name, failure=false) {
+    if (!name) name = moment().format('YYYY-MM-DDTH:mm:ss');
+    const path = './reports/screenshot/';
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recursive: true });
+    }
+
+    if (failure) {
+      name = name + '_fail';
+    }
+    name = name.replace(/ /g, '_') + '.png';
+    browser.saveScreenshot( path + name);
+    const data = fs.readFileSync(`${path}/${name}`);
+    allure.addAttachment(name, data, 'image/png');
+  }
+}
+
+module.exports = Utilities;
