e6c77260f7a4676d854627731d204bd1c76149de
Naresh Sekar <naresh@madstreetden.com>
2020-07-23 22:08:18 +0530

    adding logs

--- test/specs/example.spec.js
+++ test/specs/example.spec.js
@@ -4,7 +4,9 @@ const SecurePage = require('../pageobjects/secure.page');
 describe('My Login application', () => {
   it('should login with valid credentials', () => {
     LoginPage.open();
+    console.log('waiting ....')
     browser.pause(10000)
+    console.log('after 10 seconds waiting ....')
     LoginPage.login('tomsmith', 'SuperSecretPassword!');
     expect(SecurePage.flashAlert).toBeExisting();
     expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
