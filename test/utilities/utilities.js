e1535c26a89d0f331a65c661211ec67d0cfe52af
Naresh Sekar <naresh@madstreetden.com>
2020-07-25 00:43:13 +0530

    completed with secrets manager functionality with circleci integration

--- test/utilities/utilities.js
+++ test/utilities/utilities.js
@@ -32,7 +32,6 @@ class Utilities {
       return path;
     } else {
       fs.writeFileSync(path, process.env.privatekey);
-      console.log('((((((((((((((((((((((((((((((((' + process.env.privatekey);
       return path;
     }
   }
